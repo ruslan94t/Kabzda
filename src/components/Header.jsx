fea525c60a021726b58698c9dfb0621d1a39ba85
Ruslan <lpseeen@gmail.com>
2024-05-10 00:59:50 +0300

    gitKK

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='header'>
+       <img src='https://seeklogo.com/images/C/company-name-logo-C74A7D6F5A-seeklogo.com.png'/>
+      </header>
+  )
+}
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='content'>
+    <div>
+    <img src="https://c0.wallpaperflare.com/preview/282/489/126/beach-exotic-holiday-horizon.jpg" />
+    </div>
+    <div> ava + descriptio</div>
+    <div>
+      My Post 
+      <div>New Post</div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+        <div>Post 3</div>
+      </div>
+    </div>
+    
+  </div>
+  )
+}
