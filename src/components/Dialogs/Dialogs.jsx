3d47b6a54d3ed523b2c9e60da6842848552e2d81
Ruslan <lpseeen@gmail.com>
2024-06-06 21:56:31 +0300

    21 Dialogs Verstka

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import s from './Dialogs.modules.css'
+import s from './Dialogs.module.css'
 
 
 
 export default function Dialogs() {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+      <div className={s.dialog+' '+s.active}>
+          Dimych
+        </div>
+        <div className={s.dialog}>
+          Sveta
+        </div>
+        <div className={s.dialog}>
+          Sasha
+        </div>
+        <div className={s.dialog}>
+          Valera
+        </div>
+        <div className={s.dialog}>
+          Andrey
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>hi</div>
+        <div className={s.message}>It kama</div>
+        <div className={s.message}>Yo You</div>
+      </div>
+
     </div>
   )
 }
